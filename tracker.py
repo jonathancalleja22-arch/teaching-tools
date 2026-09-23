a44b73b79a3103886d760ce34751710af1f42235
Jonathan <jonathan.calleja.22@gmail.com>
2026-09-23 13:53:26 +0700

    Adding conditional statements to tracker.py

--- tracker.py
+++ tracker.py
@@ -1,7 +1,7 @@
 student_name = "Mr. Example"
 lesson_date = "18/09/2569"
-duration_hrs = 1.0
-fee_baht = 200
+duration_hrs = 3.0
+fee_baht = 1800
 online_or_inperson = "online"
 paid = True
 
@@ -14,3 +14,20 @@ print("Lesson Fee (baht): ", fee_baht)
 print("Online/in-person: ", online_or_inperson)
 print("Paid?: ", paid )
 print("Rate per Hour: ", rate_per_hour)
+
+if paid == False:
+    print("Payment reminder needed")
+else:
+    print("Payment received")
+
+if rate_per_hour >= 400:
+    print("Premium rate")
+elif rate_per_hour>= 250:
+    print("Standard Rate")
+else:
+    print("Below standard rate - review")
+
+if duration_hrs > 2 and paid == True:
+    print("Long Session")
+
+
